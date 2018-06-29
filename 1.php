811fe84976801852888d54039cb096641d8ba382
GuoLuLu <grilas@163.com>
2018-06-29 18:08:15 +0800

    me

---
+++ 1.php
@@ -0,0 +1,64 @@
+<?php
+
+echo "<hr>";
+$str = "不乱于心，不困于情。不畏将来，不念过往。如此，安好。";
+echo mb_substr($str, 2, 4, 'utf-8');
+
+echo "<hr>";
+
+
+
+
+/**
+ * utf8编码字符串截取无乱码
+ * @param $str string 要处理的字符串
+ * @param $start int 从哪个位置开始截取
+ * @param $length int 要截取字符的个数
+ * @return string 截取后得到的字符串
+*/
+/*
+    /u 表示按unicode(utf-8)匹配（主要针对多字节比如汉字）
+    /i 表示不区分大小写（如果表达式里面有 a， 那么 A 也是匹配对象）
+    /s 表示将字符串视为单行来匹配
+    preg_split() 通过一个正则表达式分隔给定字符串。
+    array preg_split ( string $pattern , string $subject [, int $limit = -1 [, int $flags = 0 ]] )
+
+    $pattern: 用于搜索的模式，字符串形式。正则
+    $subject: 输入字符串。
+    $limit: 可选，如果指定，将限制分隔得到的子串最多只有limit个，返回的最后一个 子串将包含所有剩余部分。
+        limit值为-1， 0或null时都代表"不限制"， 作为php的标准，你可以使用null跳过对flags的设置。
+    $flags: 可选，可以是任何下面标记的组合(以位或运算 | 组合)：
+        PREG_SPLIT_NO_EMPTY: 如果这个标记被设置， preg_split() 将进返回分隔后的非空部分。
+        PREG_SPLIT_DELIM_CAPTURE: 如果这个标记设置了，用于分隔的模式中的括号表达式将被捕获并返回。
+        PREG_SPLIT_OFFSET_CAPTURE: 如果这个标记被设置, 对于每一个出现的匹配返回时将会附加字符串偏移量. 
+    注意：这将会改变返回数组中的每一个元素, 使其每个元素成为一个由第0 个元素为分隔后的子串，第1个元素为该子串在subject 中的偏移量组成的数组。
+
+    array_slice
+    array array_slice ( array $array , int $offset [, int $length = NULL [, bool $preserve_keys = false ]] )
+    返回数组中指定下标offset和长度length的子数组切片。
+    
+    
+*/
+
+function substr_utf8($str,$start,$length = null)
+{
+    $sep = "";
+    $arr = array_slice(preg_split("//u", $str,-1,PREG_SPLIT_NO_EMPTY), $start,$length);
+    return join($sep,$arr);
+}
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
--- common/func.php
+++ common/func.php
@@ -6,6 +6,142 @@ $current_dir = str_replace('\\',"/",$current_dir);
 define('MY_PATH',$current_dir);
 
 
+/**
+ * utf8编码字符串截取无乱码
+ * @param $str string 要处理的字符串
+ * @param $start int 从哪个位置开始截取
+ * @param $length int 要截取字符的个数
+ * @return string 截取后得到的字符串
+*/
+function substr_utf8($str,$start,$length = null)
+{
+    $sep = "";
+    $arr = array_slice(preg_split("//u", $str,-1,PREG_SPLIT_NO_EMPTY), $start,$length);
+    return join($sep,$arr);
+}
+
+
+/**
+ * 循环删除目录和文件
+ * @param string $dir_name
+ * @return bool
+ */
+function delete_dir_file($dir_name)
+{
+    $result = false;
+    if (is_dir($dir_name)) {
+        if ($handle = opendir($dir_name)) {
+            while (false !== ($item = readdir($handle))) {
+                if ($item != '.' && $item != '..') {
+                    if (is_dir($dir_name . DS . $item)) {
+                        delete_dir_file($dir_name . DS . $item);
+                    } else {
+                        unlink($dir_name . DS . $item);
+                    }
+                }
+            }
+            closedir($handle);
+            if (rmdir($dir_name)) {
+                $result = true;
+            }
+        }
+    }
+
+    return $result;
+}
+
+
+/**
+ * 判断是否为手机访问
+ * @return  boolean
+ */
+function is_mobile()
+{
+    static $is_mobile;
+
+    if (isset($is_mobile)) {
+        return $is_mobile;
+    }
+
+    if (empty($_SERVER['HTTP_USER_AGENT'])) {
+        $is_mobile = false;
+    } elseif (strpos($_SERVER['HTTP_USER_AGENT'], 'Mobile') !== false
+        || strpos($_SERVER['HTTP_USER_AGENT'], 'Android') !== false
+        || strpos($_SERVER['HTTP_USER_AGENT'], 'Silk/') !== false
+        || strpos($_SERVER['HTTP_USER_AGENT'], 'Kindle') !== false
+        || strpos($_SERVER['HTTP_USER_AGENT'], 'BlackBerry') !== false
+        || strpos($_SERVER['HTTP_USER_AGENT'], 'Opera Mini') !== false
+        || strpos($_SERVER['HTTP_USER_AGENT'], 'Opera Mobi') !== false
+    ) {
+        $is_mobile = true;
+    } else {
+        $is_mobile = false;
+    }
+
+    return $is_mobile;
+}
+
+
+ /**
+ * 检查是否是手机端
+ * 
+ */
+function ispc() {
+    // 如果有HTTP_X_WAP_PROFILE则一定是移动设备
+    if (isset ($_SERVER['HTTP_X_WAP_PROFILE']))
+        return true;
+
+    //此条摘自TPM智能切换模板引擎，适合TPM开发
+    if(isset ($_SERVER['HTTP_CLIENT']) &&'PhoneClient'==$_SERVER['HTTP_CLIENT'])
+        return true;
+    //如果via信息含有wap则一定是移动设备,部分服务商会屏蔽该信息
+    if (isset ($_SERVER['HTTP_VIA']))
+    //找不到为flase,否则为true
+        return stristr($_SERVER['HTTP_VIA'], 'wap') ? true : false;
+    //判断手机发送的客户端标志,兼容性有待提高
+    if (isset ($_SERVER['HTTP_USER_AGENT'])) {
+        $clientkeywords = array(
+        'nokia','sony','ericsson','mot','samsung','htc','sgh','lg','sharp','sie-','philips','panasonic','alcatel','lenovo','iphone','ipod','blackberry','meizu','android','netfront','symbian','ucweb','windowsce','palm','operamini','operamobi','openwave','nexusone','cldc','midp','wap','mobile'
+        );
+        //从HTTP_USER_AGENT中查找手机浏览器的关键字
+        if (preg_match("/(" . implode('|', $clientkeywords) . ")/i", strtolower($_SERVER['HTTP_USER_AGENT']))) {
+            return true;
+        }
+    }
+    //协议法，因为有可能不准确，放到最后判断
+    if (isset ($_SERVER['HTTP_ACCEPT'])) {
+    // 如果只支持wml并且不支持html那一定是移动设备
+    // 如果支持wml和html但是wml在html之前则是移动设备
+        if ((strpos($_SERVER['HTTP_ACCEPT'], 'vnd.wap.wml') !== false) && (strpos($_SERVER['HTTP_ACCEPT'], 'text/html') === false || (strpos($_SERVER['HTTP_ACCEPT'], 'vnd.wap.wml') < strpos($_SERVER['HTTP_ACCEPT'], 'text/html')))) {
+            return true;
+        }
+    }
+    return false;
+}
+
+ /**
+ * 返回加密后的密码字符串
+ * @param unknown_type $password
+ */
+function getPwdEncodeString($password){
+    return md5(md5($password).substr($password,0,3));
+}
+
+
+/**
+ * 生成json中文显示方法
+ * @param json_string
+ * @return string
+*/
+function decodeUnicode($str){
+    return preg_replace_callback('/\\\\u([0-9a-f]{4})/i',
+    create_function(
+      '$matches',
+      'return mb_convert_encoding(pack("H*", $matches[1]), "UTF-8", "UCS-2BE");'
+    ),
+    $str);
+}
+
 
 /**
  * 将数组写入文件
